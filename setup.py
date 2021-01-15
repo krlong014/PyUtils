d1c9f61b67c491126e7707099f1bc3f248f230e6
Katharine Long <krlong014@gmail.com>
2021-01-15 13:35:22 -0600

    Package setup

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="PyUtils", # Replace with your own username
+    version="0.1",
+    author="Katharine Long",
+    author_email="katharine.long@ttu.edu",
+    description="Simple development utilities",
+    long_description="Profilers, Loggers, Tabbers, etc",
+    long_description_content_type="text/markdown",
+    url="https://github.com/krlong014/PyPDE",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: LGPL License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
